06a0c9110b09c9c694a8b3637942d868aee35215
keet <keetsta@icloud.com>
2024-12-18 23:28:29 +0500

    Initial Commit

---
+++ AnimatedText/AnimatedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTextApp.swift
+//  AnimatedText
+//
+//  Created by keet on 18.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
