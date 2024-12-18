4deb6fcd40ecdf91cfcd5f23811e7f78350a8da3
keet <keetsta@icloud.com>
2024-12-19 00:58:40 +0500

    Final

--- AnimatedText/ContentView.swift
+++ AnimatedText/ContentView.swift
@@ -8,12 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animate = false
+    @State private var text = "Open"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if !animate { Spacer() }
+            
+            HStack {
+                if !animate { Spacer() }
+                
+                RoundedRectangle(cornerRadius: 16)
+                    .frame(width: animate ? 350 : 100, height: animate ? 400 : 50)
+                    .foregroundStyle(.blue)
+                    .overlay {
+                        HStack {
+                            if animate {
+                                Image(systemName: "arrowshape.backward.fill")
+                            }
+                            
+                            Text(text)
+                                .bold()
+                                .transition(.identity)
+                                .onTapGesture {
+                                    if animate {
+                                        text = "Open"
+                                    } else {
+                                        text = "Close"
+                                    }
+                                    
+                                    withAnimation(.default) {
+                                        animate.toggle()
+                                    }
+                                }
+                        }
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: animate ? .topLeading : .center)
+                        .padding()
+                    }
+                    .onTapGesture {
+                        if !animate {
+                            text = "Close"
+                            
+                            withAnimation(.default) {
+                                animate.toggle()
+                            }
+                        }
+                    }
+            }
         }
         .padding()
     }
